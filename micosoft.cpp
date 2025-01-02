9541a6bff7ec7bfdcfafc7550f09feb9042b84a8
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-01-02 09:21:37 +0530

    changes

--- micosoft.cpp
+++ micosoft.cpp
@@ -96,3 +96,44 @@ public:
         }
     }
 };
+
+
+// 4 (easy -Count the Number of Incremovable Subarrays I)
+
+
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int cnt = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                bool flag = true;
+                int last = -1;
+
+                for (int k = 0; k < i; k++) {
+                    if (last >= nums[k]) {
+                        flag = false;
+                        break;
+                    }
+                    last = nums[k];
+                }
+
+                for (int k = j + 1; k < n; k++) {
+                    if (last >= nums[k]) {
+                        flag = false;
+                        break;
+                    }
+                    last = nums[k];
+                }
+
+                if (flag) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
