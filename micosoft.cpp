0abcb8736beec132ebd6ed6608f0280e90a27808
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-01-09 18:31:08 +0530

    Update micosoft.cpp

--- micosoft.cpp
+++ micosoft.cpp
@@ -192,3 +192,24 @@ public:
         return subarray;
     }
 };
+
+//7(187. Repeated DNA Sequences)
+
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string>ans;
+        unordered_map<string,int>mpp;
+        for(int i=0;i+10<=s.length();i++){
+            mpp[s.substr(i,10)]++;
+        }
+
+        for(auto& i:mpp){
+            if(i.second>1){
+                ans.push_back(i.first);
+            }
+        }
+       return ans;
+
+    }
+};
